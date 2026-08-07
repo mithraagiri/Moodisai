61e344808104c73e9996d803f01ec758b0132e37
Mithraa G <130753099+mithraagiri@users.noreply.github.com>
2026-08-07 12:29:44 +0530

    Add Vercel domain to CORS allowed origins

--- backend/main.py
+++ backend/main.py
@@ -14,6 +14,8 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://192.168.1.10:5173",
+        "https://moodmuse-eight.vercel.app",
+        "https://moodmuse-backend-npfm.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
